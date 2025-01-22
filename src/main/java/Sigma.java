0b0f33507520257b7393c7f7e166e85172ae41ce
Lee Chen Xuan <e1129901@u.nus.edu>
2025-01-22 20:24:04 +0800

    added echoing

--- src/main/java/Sigma.java
+++ src/main/java/Sigma.java
@@ -4,7 +4,12 @@ import java.util.Scanner;
 public class Sigma {
     private String name = "Sigma";
     private String reply_prefix = name + ": ";
+    private Scanner sc;
 
+    public Sigma() {
+        sc = new Scanner(System.in);
+    }
+    
     private void set_Name(String name) {
         this.name = name;
     }
@@ -19,13 +24,18 @@ public class Sigma {
     }
 
     private void await_reply() {
-        Scanner reply = new Scanner(System.in);
+        String reply = sc.nextLine();
+        if (reply.equals("bye")) {
+            exit();
+        } else {
+            System.out.println(reply_prefix + reply);
+            await_reply();
+        }
     }
 
     private void start() {
         greet();
-
-        exit();
+        await_reply();
     }
 
     public static void main(String[] args) {
