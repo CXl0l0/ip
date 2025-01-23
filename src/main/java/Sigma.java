1dee7d48f5b7c011f0a1e6c9133e1d0906b93b4e
Lee Chen Xuan <e1129901@u.nus.edu>
2025-01-23 11:48:51 +0800

    Added marking done/not done feature for tasks

--- src/main/java/Sigma.java
+++ src/main/java/Sigma.java
@@ -10,18 +10,23 @@ public class Sigma {
     private void set_Name(String name) {
         this.name = name;
     }
+    
+    public Sigma() {
+        sc = new Scanner(System.in);
+        list = new ArrayList<>();
+    }
 
     //Scanner
     private Scanner sc;
 
     //List
-    ArrayList<String> list;
+    ArrayList<Task> list;
     private void show_list() {
         line();
         System.out.println(reply_prefix + "Stop slacking and lock in.");
         for (int i = 0; i < list.size(); i++) {
-            String todo = list.get(i);
-            System.err.println((i + 1) + ". " + todo);
+            Task todo = list.get(i);
+            System.err.println((i + 1) + ". "+ todo.toString());
         }
         line();
         await_reply();
@@ -29,19 +34,35 @@ public class Sigma {
 
     private void add_list(String todo) {
         line();
-        list.add(todo);
-        System.out.println(reply_prefix + "I will remember that for you.");
-        System.out.println("added: " + todo);
+        Task task = new Task(todo);
+        list.add(task);
+        System.out.println(reply_prefix + "Aight, I will remember that for you.");
+        System.out.println("added: " + task.getTaskName());
         line();
         await_reply();
     }
 
-    public Sigma() {
-        sc = new Scanner(System.in);
-        list = new ArrayList<>();
+    private void mark_done(int i) {
+        Task todo = list.get(i - 1);
+        todo.setDone(true);
+        line();
+        System.out.println("Good job bro, keep the grind going!");
+        System.out.println(todo);
+        line();
+        await_reply();
     }
-    
 
+    private void mark_undone(int i) {
+        Task todo = list.get(i - 1);
+        todo.setDone(false);
+        line();
+        System.out.println("Come on bruh, focus!");
+        System.out.println(todo);
+        line();
+        await_reply();
+    }
+
+    //Dialogue functions
     private void greet() {
         line();
         System.out.println(reply_prefix + "What's up, I'm " + name);
@@ -59,13 +80,26 @@ public class Sigma {
     private void await_reply() {
         System.out.print("You: ");
         String reply = sc.nextLine();
-        switch (reply) {
+        //Token reading solution below inspired by https://www.youtube.com/watch?v=lGHlFaF0F44
+        String[] tokens = reply.split(" ");
+        String command = tokens[0];
+        switch (command) {
             case "bye":
                 exit();
                 break;
             case "list":
                 show_list();
                 break;
+            case "mark": {
+                int index = Integer.parseInt(tokens[1]);
+                mark_done(index);
+                break;
+            }
+            case "unmark": {
+                int index = Integer.parseInt(tokens[1]);
+                mark_undone(index);
+                break;
+            }
             default:
                 add_list(reply);
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,32 @@
+
+public class Task {
+    private String taskName;
+    private boolean done;
+
+    public Task(String taskName) {
+        this.taskName = taskName;
+        this.done = false;
+    }
+
+    public Task(String taskName, boolean done) {
+        this.taskName = taskName;
+        this.done = done;
+    }
+
+    public String getTaskName() {
+        return this.taskName;
+    }
+
+    public boolean isDone() {
+        return this.done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + (done ? "X" : " ") + "] " + taskName;
+    }
+}
